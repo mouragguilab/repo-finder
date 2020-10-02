a0c569beffa1aaf47df2a1d6693d2228b94e6961
Guilherme Moura <mouraggui@gmail.com>
2020-10-02 15:38:15 -0300

    global components folder

---
+++ src/app/components/Content/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+  Container,
+} from './styles'
+
+function Content({ children }) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
+}
+
+export default Content;
---
+++ src/app/components/Content/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  border: 1px solid #e1e4e8;
+  border-radius: 6px;
+  width: 90%;
+  max-width: 600px;
+  min-height: 80vh;
+  padding: 16px;
+  margin: 8px 0px;
+  box-sizing: content-box;
+  background-color: #fff;
+`;
